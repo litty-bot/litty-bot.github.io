24b21f134883c6ed1fdfc6a758581f447fb6acb0
realtechnerd <send.nithil@gmail.com>
2021-04-10 00:05:16 -0400

    Added 'Vote' to navbar and fetched server on home.

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { BOT_ID } from "../config";
 
 export default function NavbarComponent() {
   const inviteLink =
     "https://ptb.discord.com/api/oauth2/authorize?client_id=808034778393608244&permissions=8&scope=bot";
+  const topGGLink = `https://top.gg/bot/${BOT_ID}/vote`;
   return (
     <Navbar style={{ backgroundColor: "#23272A" }} variant="dark" expand="lg">
       <Container>
@@ -24,6 +26,9 @@ export default function NavbarComponent() {
             <Link className="hover:no-underline nav-link" to="/commands">
               Commands
             </Link>
+            <Nav.Link href={topGGLink} target="_blank">
+              Vote
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   CORS_SERVER_URL: "https://lit-cors.herokuapp.com/",
   LIT_BOT_SERVER_URL: "https://lit-bot-serv.herokuapp.com/",
+  BOT_ID: "808034778393608244",
 };
