aedaab4ceb61e07b0f6fb9a86e540da555a70f05
AdvaithM26 <65194982+AdvaithM26@users.noreply.github.com>
2021-04-23 10:19:48 -0400

    chnage server config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   CORS_SERVER_URL: "https://lit-cors.herokuapp.com/",
-  LIT_BOT_SERVER_URL: "https://lit-bot-serv.herokuapp.com/",
+  LIT_BOT_SERVER_URL: "https://litbot.advaithm26.repl.co/",
 };
