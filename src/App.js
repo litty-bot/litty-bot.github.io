55613fcfa8974283ba7cf84b6a48b22369085727
AdvaithM26 <65194982+AdvaithM26@users.noreply.github.com>
2021-05-19 18:55:44 -0400

    fixy fixy

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import { LIT_BOT_SERVER_URL, CORS_SERVER_URL } from "./config.json";
 function App() {
   fetch(`${CORS_SERVER_URL}${LIT_BOT_SERVER_URL}`);
   return (
-    <>
-      <Router basename={process.env.PUBLIC_URL}>
-        <NavbarComponent />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/commands" exact component={Commands} />
-        </Switch>
-      </Router>
-    </>
+    <Router basename={process.env.PUBLIC_URL}>
+      <NavbarComponent />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/commands" exact component={Commands} />
+      </Switch>
+    </Router>
   );
 }
 
